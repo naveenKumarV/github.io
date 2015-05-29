9bb74b70a6189cbc0f13e58cba80b1e0670e1971
naveenKumarV <naveenkumarvedurupaka@gmail.com>
2015-05-29 16:03:20 +0530

    Everything is fine.Still the code has to be made readable.

--- main.js
+++ main.js
@@ -6,11 +6,31 @@ function append(val)
 {
 	document.getElementById("screen").value+=val;
 }
+function process(str)
+{
+	search=new Array('sin','cos','tan','log','ln','sqrt','!');
+	repl=new Array('Math.sin','Math.cos','Math.tan','1/2.302585092994*Math.log','Math.log','Math.sqrt','fact');
+	for(var i=0;i<search.length;++i)
+	{
+		str=str.split(search[i]).join(repl[i]);
+	}
+	console.log(str);
+	return str;
+} 
+function fact(n)
+{
+	var f=1;
+	for(var i=1;i<=n;++i)
+	{
+		f*=i;
+	}
+	return f;
+}
 function compute() 
 { 
-	try 
+	try
 	{ 
-	  display(eval(document.getElementById("screen").value)); 
+	  display(eval(process(document.getElementById("screen").value))); 
 	} 
 	catch(e) 
 	{
